5d394afaeda54e48bbe6a8bf653276f42b3672b4
Okan Binli <okanbinli@gmail.com>
2022-06-15 11:07:41 +0300

    Initial commit

---
+++ lib/lp2wiegand.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "lp2wiegand/version"
+
+module Lp2wiegand
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/lp2wiegand/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Lp2wiegand
+  VERSION = "0.1.0"
+end
